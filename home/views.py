1739fee95da9afc26b39aea74637755e4a21a949
Syed Izhan <58202306+izicodez@users.noreply.github.com>
2022-03-28 20:59:37 +0400

    Added a contact page
    Contact.html has been added

--- home/views.py
+++ home/views.py
@@ -7,13 +7,12 @@ def index(request):
         'var2':'40.00'
     }
     return render(request, 'index.html', context)
-    #return HttpResponse("THis is home page")
 
 def about(request):
     return render(request, 'about.html')     
 
 def contact(request):
-    return HttpResponse("THis is contact page")    
+    return render(request, 'contact.html')    
 
 def base(request):
     return render(request, 'base.html') 
